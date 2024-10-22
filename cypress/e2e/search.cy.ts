2c932e3f92d6cec8dfaa548ed35497b079d2542d
José Victor <josevictornascimento2016@gmail.com>
2024-10-22 19:27:44 -0300

    test: create custom command for search by query

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -4,7 +4,7 @@ describe('add product to cart', () => {
   })
 
   it('should be able to search for products', () => {
-    cy.get('input[name=q]').type('moletom').parent('form').submit()
+    cy.searchByQuery('moletom')
 
     cy.location('pathname').should('include', '/search')
     cy.location('search').should('include', 'q=moletom')
